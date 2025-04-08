d369beb59d99591ef60c65d22815551923dcdb41
upadhyaykartikeya <ukartik451@gmail.com>
2025-04-08 11:02:52 +0530

    added JS file for updating weather according to the city

---
+++ script.js
@@ -0,0 +1,22 @@
+const apiKey = "659c43f8913ebde4f306be84e0a6be90";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+
+searchbtn.addEventListener("click", ()=> {
+    checkWeather(searchbox.value);
+})
